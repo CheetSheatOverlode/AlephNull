dff2694f9d3eb46675e723a50529faf0f7873f5c
Eric Zou <cheetsheatoverlode@gmail.com>
2020-05-12 14:51:47 -0400

    Added files, added version display in shell, added AlephNull module.

---
+++ shell.py
@@ -0,0 +1,12 @@
+import AlephNull
+
+print("AlephNull 0.1.2")
+print("AlephNull created 2020 by Eric Zou and the Arch-X Corporation.")
+print("============Shell============")
+while True:
+    text = input('AlephNull>>>')
+    result, error = AlephNull.run('<stdin>', text)
+
+    if error: print(error.as_string())
+    else: print(result)
+input("Error detected. Press any key to exit...")
